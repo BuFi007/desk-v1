e52744a471868408f842ecf01b8b3dbe927ef71f
Tomas Oliver <tomi204@outlook.com>
2024-12-11 00:27:35 -0300

    push

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,44 @@
+import "@bu/ui/globals.css";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { Provider as AnalyticsProvider } from "@bu/analytics/client";
+import { cn } from "@bu/ui/cn";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+const DepartureMono = localFont({
+  src: "../fonts/DepartureMono-Regular.woff2",
+  variable: "--font-departure-mono",
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://bu.run"),
+  title: "Create bu",
+  description:
+    "A free, open-source starter kit for your next project, built with insights from Midday.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          `${DepartureMono.variable} ${GeistSans.variable} ${GeistMono.variable}`,
+          "antialiased dark",
+        )}
+      >
+        <Header />
+        {children}
+        <Footer />
+
+        <AnalyticsProvider />
+      </body>
+    </html>
+  );
+}
