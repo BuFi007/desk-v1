0d75f569b4a161d343df2859a79fed65f3b6a8a3
CriptoPoeta <tomas.cordero.esp@gmail.com>
2024-12-11 00:25:17 -0300

    scaffold

---
+++ apps/app/src/components/sign-out.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { createClient } from "@bu/supabase/client";
+import { Button } from "@bu/ui/button";
+import { Icons } from "@bu/ui/icons";
+
+export function SignOut() {
+  const supabase = createClient();
+
+  const handleSignOut = () => {
+    supabase.auth.signOut();
+  };
+
+  return (
+    <Button
+      onClick={handleSignOut}
+      variant="outline"
+      className="font-mono gap-2 flex items-center"
+    >
+      <Icons.SignOut className="size-4" />
+      <span>Sign out</span>
+    </Button>
+  );
+}
