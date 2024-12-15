8aec2b7d47115a5446edfd7ec6d21fde4156b634
CriptoPoeta <tomas.cordero.esp@gmail.com>
2024-12-15 07:29:58 -0300

    fix(build)

--- apps/web/src/sections/header.tsx
+++ apps/web/src/sections/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="dark:bg-secondaryBlack inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <div className="mx-auto w-container max-w-full px-5 py-[110px] text-center lg:py-[150px]">
         <h1 className="text-3xl font-heading md:text-4xl lg:text-5xl">
-          Bu 👻 don't be scared,<br />be smart.
+          Bu 👻 don&apos;t be scared,<br />be smart.
         </h1>
         <p className="my-12 mt-8 text-lg font-normal leading-relaxed md:text-xl lg:text-2xl lg:leading-relaxed">
           Spooky Finance, Accounting and DeFi made easy.
