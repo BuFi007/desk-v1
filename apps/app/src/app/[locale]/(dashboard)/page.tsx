35c209146a7ccadc6bb9749532db05fe74018d5a
CriptoPoeta <tomas.cordero.esp@gmail.com>
2024-12-15 06:41:15 -0300

    feat(merge)

--- apps/app/src/app/[locale]/(dashboard)/page.tsx
+++ apps/app/src/app/[locale]/(dashboard)/page.tsx
@@ -1,12 +1,6 @@
-import { SignOut } from "@/components/sign-out";
-import { getI18n } from "@/locales/server";
 import { getUser } from "@bu/supabase/queries";
 import { InvoiceContainer } from "@/components/peanut-zk-invoices/invoice-container";
-import { Suspense } from "react";
-import { LoanCTA } from "@/components/loan-cta";
-import { LoanCTASkeleton } from "@/components/loan-cta/skeleton";
-import ScratchCard from "@/components/scratch-card";
-
+import { CreateRegularPaymentSheet } from "@/components/createRegularPayment";
 export default async function Page() {
   const { data } = await getUser();
 
@@ -18,27 +12,15 @@ export default async function Page() {
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p>{t("welcome", { name: data?.user?.email })}</p>
-
-        <SignOut />
+    <div className="min-h-screen relative p-4">
+      <div className="absolute top-4 right-4 z-10">
+        <InvoiceContainer userData={userData} />
       </div>
-      {/* Main content grid */}
-      {/* <div className="h-screen flex flex-col items-center justify-center">
-        <div className="w-full max-w-7xl">
-          <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-            <div className="lg:col-span-1 flex items-center justify-center">
-              <ScratchCard />
-            </div>
-            <div className="lg:col-span-2">
-              <Suspense fallback={<LoanCTASkeleton />}>
-                <LoanCTA />
-              </Suspense>
-            </div>
-          </div>
+      <div className="h-screen w-screen flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <CreateRegularPaymentSheet />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
