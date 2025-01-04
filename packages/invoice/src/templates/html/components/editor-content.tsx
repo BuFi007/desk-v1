b9bfb8dc5f2391f28ca9c70bead28759ef86e251
Tomas Cordero <tomas.cordero.esp@gmail.com>
2025-01-03 19:43:42 -0500

    feat(fix): biome run check

--- packages/invoice/src/templates/html/components/editor-content.tsx
+++ packages/invoice/src/templates/html/components/editor-content.tsx
@@ -2,12 +2,12 @@ import type { EditorDoc } from "../../types";
 import { formatEditorContent } from "../format";
 
 export function EditorContent({ content }: { content?: JSON }) {
-	if (!content) {
-		return null;
-	}
-	return (
-		<div className="font-mono leading-4">
-			{formatEditorContent(content as unknown as EditorDoc)}
-		</div>
-	);
+  if (!content) {
+    return null;
+  }
+  return (
+    <div className="font-mono leading-4">
+      {formatEditorContent(content as unknown as EditorDoc)}
+    </div>
+  );
 }
